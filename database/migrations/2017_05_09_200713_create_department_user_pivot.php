051964f73139d9685f071b9a9dec0c7974375148
Brieuc Barthélemy <bri@fatcha.be>
2017-05-09 22:36:29 +0200

    departmenet

--- database/migrations/2017_05_09_200713_create_department_user_pivot.php
+++ database/migrations/2017_05_09_200713_create_department_user_pivot.php
@@ -14,27 +14,27 @@ class CreateDepartmentUserPivot extends Migration
     public function up()
     {
 
-        if (Schema::hasColumn('users', 'department_id')) {
-            Schema::table('users', function (Blueprint $table) {
-                $table->dropForeign(['department_id']);
-                $table->dropColumn('department_id');
-
+        if (!Schema::hasColumn('company_user', 'department_id')) {
+            Schema::table('company_user', function (Blueprint $table) {
+                $table->unsignedInteger('department_id')->nullable()->after('role');
+                $table->foreign('department_id')->references('id')->on('departments');
             });
         }
-        Schema::create('department_user', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
 
-            $table->increments('id');
-            $table->unsignedInteger('user_id');
-            $table->unsignedInteger('department_id');
-
-            $table->string('role')->nullable();;
-
-            $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('department_id')->references('id')->on('departments');
-        });
+//        Schema::create('department_user', function (Blueprint $table) {
+//            $table->engine = 'InnoDB';
+//
+//            $table->increments('id');
+//            $table->unsignedInteger('user_id');
+//            $table->unsignedInteger('department_id');
+//
+//            $table->string('role')->nullable();;
+//
+//            $table->timestamps();
+//
+//            $table->foreign('user_id')->references('id')->on('users');
+//            $table->foreign('department_id')->references('id')->on('departments');
+//        });
 
 
     }
@@ -47,6 +47,13 @@ class CreateDepartmentUserPivot extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('department_user');
+        if (Schema::hasColumn('company_user', 'department_id')) {
+            Schema::table('company_user', function (Blueprint $table) {
 
+                $table->dropForeign('department_id');
+                $table->dropColumn('department_id');
+            });
+        }
     }
 }
