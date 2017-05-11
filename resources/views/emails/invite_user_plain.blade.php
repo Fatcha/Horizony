e62bf2d14bee7c4506a65c532b72fb67b6b34c7e
Brieuc Barthélemy <bri@fatcha.be>
2017-05-11 17:10:39 +0200

    Mailgun added

--- resources/views/emails/invite_user_plain.blade.php
+++ resources/views/emails/invite_user_plain.blade.php
@@ -1,1 +1,6 @@
-youpiiiiiiiiiiii
+
+    {{ config('app.name', 'Laravel') }}
+    {{$company->name}} invited you on Horizony
+    You 'll see the planning of the company
+
+        Go to your Horizon by clicking here :  {{$user->generateUrlConnexion()}}
