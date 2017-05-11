c84b44d4150f2e2e175cd8a5119d1f92f2b71bfe
Brieuc Barthélemy <bri@fatcha.be>
2017-05-11 17:01:59 +0200

    Mailgun added

--- resources/views/emails/invite_user.blade.php
+++ resources/views/emails/invite_user.blade.php
@@ -1,1 +1,11 @@
-Hello test me
+@extends('emails.layout')
+
+@section('content')
+    {{ config('app.name', 'Laravel') }}<br>
+    {{$company->name}} invited you on Horizony<br>
+    You 'll see the planning of the company<br>
+
+    <a href="{{$user->generateUrlConnexion()}}">Go to your Horizon</a>
+
+
+@endsection
