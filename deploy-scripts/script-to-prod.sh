2f0ac830a0d559e5ca98990bb2e32c10cc2181dd
bri <bri@fatcha.be>
2017-07-05 13:13:29 +0200

    add stage environment

--- deploy-scripts/script-to-prod.sh
+++ deploy-scripts/script-to-prod.sh
@@ -1,19 +1,55 @@
-target_folder="/home/horizony/prod/"
-src_folder=$1
-echo "-----------------------"
-echo "User name $USER "
-echo "-----------------------"
-cd ${src_folder}
-#npm & gulp
-npm install
-npm build
 
-echo "-----------------------"
-echo "|    Composer Update"
-echo "-----------------------"
+# first minify css
+cd /var/lib/jenkins/workspace/horizony-staging
+npm run prod
+
+yes | cp -rf /var/lib/jenkins/workspace/horizony-staging/* /home/horizony/prod/
+
+###################
+#Move to folder
+###################
+cd /home/horizony/prod/
+#remove current .env
+rm .env
+
+#copy the new one
+yes | cp  /var/lib/jenkins/workspace/horizony-staging/.env.production ./.env
+yes | cp  /var/lib/jenkins/workspace/horizony-staging/.htaccess.production ./public/.htaccess
+
+yes | cp  /var/lib/jenkins/workspace/horizony-staging/robots.production.txt ./public/robots.txt
+
+#composer
 composer update
 
-yes |  cp -r ${src_folder}/* ${target_folder}
+#migrate DB
+php artisan migrate
+
+chown horizony:horizony ./ -R
+chmod 775 ./storage -R
+chmod 775 ./bootstrap/cache -R
+
+
+
+
+
+
+
+#target_folder="/home/horizony/prod/"
+#src_folder=$1
+#echo "-----------------------"
+#echo "User name $USER "
+#echo "-----------------------"
+#cd ${src_folder}
+##npm & gulp
+#npm install
+#npm build
+
+#echo "-----------------------"
+#echo "|    Composer Update"
+#echo "-----------------------"
+#composer update
+
+#yes |  cp -r ${src_folder}/* ${target_folder}
 
 
 
@@ -22,28 +58,28 @@ yes |  cp -r ${src_folder}/* ${target_folder}
 ###################
 #Move to folder
 ###################
-echo "-----------------------"
-echo "Move to folder: "${target_folder}
-echo "-----------------------"
-cd ${target_folder}
+#echo "-----------------------"
+#echo "Move to folder: "${target_folder}
+#echo "-----------------------"
+#cd ${target_folder}
 
 #remove current .env
 
-echo "---> Move file for production"
-rm .env
-rm ./public/.htaccess
-rm ./public/robots.txt
+#echo "---> Move file for production"
+#rm .env
+#rm ./public/.htaccess
+#rm ./public/robots.txt
 #copy the new one
-yes |  cp  ${src_folder}/.env.production ./.env
-yes |  cp  ${src_folder}/public/.htaccess.production ./public/.htaccess
+#yes |  cp  ${src_folder}/.env.production ./.env
+#yes |  cp  ${src_folder}/public/.htaccess.production ./public/.htaccess
 
-yes |  cp  ${src_folder}/public/robots.production.txt ./public/robots.txt
+#yes |  cp  ${src_folder}/public/robots.production.txt ./public/robots.txt
 
-echo "---> migrate DB"
+#echo "---> migrate DB"
 #migrate DB
-php artisan migrate --force
+#php artisan migrate --force
 
-chown horizony:horizony ./ -R
-chmod 775 ./storage -R
-chmod 775 ./bootstrap/cache -R
+#chown horizony:horizony ./ -R
+#chmod 775 ./storage -R
+#chmod 775 ./bootstrap/cache -R
 
