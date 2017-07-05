9f01bce8b02d55e11d6fac6cacbee700eccb4758
bri <bri@fatcha.be>
2017-07-05 13:15:11 +0200

    Test stage - > production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ Auth::check() ? route('connected_dashboard') : '/' }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Laravel') }}.
                     </a>
                 </div>
 
